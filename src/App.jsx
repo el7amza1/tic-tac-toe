6612c83ca8180f2b41aa31df55741902888bf369
el7amza1 <79344270+el7amza1@users.noreply.github.com>
2024-06-23 13:24:03 +0300

    first push

--- src/App.jsx
+++ src/App.jsx
@@ -2,84 +2,70 @@ import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import { useState } from "react"
 import Log from "./components/Log"
+
 import { WINNING_COMBINATIONS } from "../src/WINNING_COMBINATIONS ";
 import GameOver from "./components/GameOver";
 
-// Function to determine the active player based on the game turns
 function deriveActivePlayer(gameTurns) {
-  let currentPlayer = 'X';
+  let currentPlayer = 'X'
 
-  // If the first move was made by 'X', it's 'O's turn next
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
-    currentPlayer = 'O';
+    currentPlayer = 'O'
   }
-  return currentPlayer;
+  return currentPlayer
 }
 
-// Define player symbols and their corresponding names
 const PLAYERS = {
-  'X': 'Player 1',
+  'X':'Player 1',
   'O': 'Player 2'
-};
+}
 
-// Initial state of the game board, a 3x3 grid filled with null
 const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
-];
-
+]
 function App() {
-  // State to track game turns
-  const [gameTurns, setGameTurns] = useState([]);
-  // State to track player names
-  const [players, setPlayers] = useState(PLAYERS);
-  // Determine the active player based on the game turns
-  const activePlayer = deriveActivePlayer(gameTurns);
+  const [gameTurns, setGameTurns] = useState([])
+  const [players, setPlayers] = useState(PLAYERS)
+  const activePlayer = deriveActivePlayer(gameTurns)
 
-  // Function to derive the current state of the game board based on the game turns
+
   function deriveGameBoard(gameTurns) {
-    // Create a deep copy of the initial game board
     let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
 
-    // Update the game board with the moves made so far
     for (const turn of gameTurns) {
       const { square, player } = turn;
-      const { row, col } = square;
+      const { row, col } = square
 
       gameBoard[row][col] = player;
     }
-    return gameBoard;
+    return gameBoard
   }
 
-  // Function to determine the winner based on the current state of the game board
   function deriveWinner(gameBoard, players) {
     let winner;
     for (const combination of WINNING_COMBINATIONS) {
-      const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
-      const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
-      const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
-      if (firstSquareSymbol &&
-          firstSquareSymbol === secondSquareSymbol &&
-          firstSquareSymbol === thirdSquareSymbol) {
-        winner = players[firstSquareSymbol];
+      const firstSuareSymbol = gameBoard[combination[0].row][combination[0].column]
+      const secondSuareSymbol = gameBoard[combination[1].row][combination[1].column]
+      const thirdSuareSymbol = gameBoard[combination[2].row][combination[2].column]
+      if (firstSuareSymbol &&
+        firstSuareSymbol === secondSuareSymbol &&
+        firstSuareSymbol === thirdSuareSymbol) {
+        winner = players[firstSuareSymbol]
       }
     }
-    return winner;
+    return winner
   }
-
-  // Get the current state of the game board
-  const gameBoard = deriveGameBoard(gameTurns);
-  // Determine if there's a winner
-  const winner = deriveWinner(gameBoard, players);
-  // Determine if the game is a draw
-  const hasDraw = gameTurns.length === 9 && !winner;
-
-  // Function to handle a square selection by the player
-  function handelSelectSquare(rowIndex, colIndex) {
-    // Update the game turns with the new move
+  const gameBoard = deriveGameBoard(gameTurns)
+  const winner = deriveWinner(gameBoard, players)
+  const hasDraw = gameTurns.length === 9 && !winner
+  function handelSelectSuare(rowIndex, colIndex) {
+    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
     setGameTurns(prevTurns => {
-      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const currentPlayer = deriveActivePlayer(prevTurns)
+
 
       const updatedTurns = [{
         square: {
@@ -87,39 +73,39 @@ function App() {
           col: colIndex
         },
         player: currentPlayer
-      }, ...prevTurns];
-      return updatedTurns;
-    });
+      }, ...prevTurns]
+      return updatedTurns
+
+    })
   }
 
-  // Function to handle game restart
   function handelRestart() {
-    setGameTurns([]);
+    setGameTurns([])
+
   }
 
-  // Function to handle player name change
   function handlePlayerNameChange(symbol, newName) {
     setPlayers((prevPlayer) => {
       return {
         ...prevPlayer,
         [symbol]: newName
-      };
-    });
+      }
+    })
   }
-
   return (
-    <main>
+    <main >
       <div id="game-container">
-        <ol id="players" className='highlight-player'>
+        <ol id="players" className='highlight-player '>
           <Player initialName={PLAYERS.X} symbol="X" isActive={activePlayer === "X"} onChangeName={handlePlayerNameChange} />
           <Player initialName={PLAYERS.O} symbol="O" isActive={activePlayer === "O"} onChangeName={handlePlayerNameChange} />
         </ol>
         {winner || hasDraw ? <GameOver winner={winner} onRestart={handelRestart} /> : ''}
-        <GameBoard board={gameBoard} onSelectSquare={handelSelectSquare} />
+        <GameBoard board={gameBoard} onSelectSuare={handelSelectSuare} />
       </div>
       <Log turns={gameTurns} />
+
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
